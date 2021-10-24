c49df17e31010f9a0a8c4981015220d469bcc18a
Saniee <asamsku10@gmail.com>
2021-10-24 14:02:56 +0200

    Getting somewhere finally

--- src/commands/getMap.ts
+++ src/commands/getMap.ts
@@ -1,6 +1,6 @@
 import { Canvas, createCanvas, loadImage } from 'canvas';
 import { Message, MessageAttachment, MessageEmbed } from 'discord.js';
-import request from 'request';
+import axios from 'axios';
 import path from 'path';
 import fs from 'fs';
 import serverConfig from '../mongodb/serverConfig';
@@ -59,177 +59,181 @@ module.exports = {
     }
 
     async function apiRequest(cache: any, mongoDB: any) {
-      const dynamicMapDataUrl = {
-        url: `${mongoDB.shard}/api/worldconquest/maps/${args[0]}/dynamic/public`,
-        headers: { 'If-None-Match': `"${cache.version}"` },
-      };
+      const dynamicMapDataUrl = `${mongoDB.shard}/api/worldconquest/maps/${args[0]}/dynamic/public`;
+
+      // headers: { 'If-None-Match': `"${cache.version}"` }
 
       try {
-        request(dynamicMapDataUrl, async function (error, response, body) {
-          if (response.statusCode == 404) {
-            msg.reply({
-              content: 'There was nothing found for the Map/Hex Specified!',
-            });
-          } else if (response.statusCode == 200) {
-            console.log(
-              `Api request for: getMap, Response Code: ${response.statusCode}, File: Not Up-to Date!`
-            );
+        const canvas = createCanvas(1024, 888);
 
-            const data = JSON.parse(body);
-
-            serverConfig.findOne(
-              { guildID: msg.guildId },
-              (err: any, mongoDB: any) => {
-                fs.writeFile(
-                  path.resolve(
-                    __dirname,
-                    `../cache/getMap/${args[0]}${mongoDB.shardName}.json`
-                  ),
-                  JSON.stringify(data, null, 2),
-                  'utf-8',
-                  (err) => {
-                    if (err) {
-                      console.log(err);
-                    } else {
-                      console.log('File: Updated!');
-                    }
-                  }
-                );
-              }
-            );
-
-            const canvas = createCanvas(1024, 888);
-            const ctx = canvas.getContext('2d');
-            const map = await loadImage(
-              path.resolve(
-                __dirname,
-                `../assets/Images/MapsPNG/Map${args[0]}.png`
-              )
-            );
-            ctx.drawImage(map, 0, 0, canvas.width, canvas.height);
-
-            for (var i = 0; i < data.mapItems.length; i++) {
-              if (data.mapItems[i].teamId == 'WARDENS') {
-                const iconWardens = await loadImage(
-                  path.resolve(
-                    __dirname,
-                    `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}${data.mapItems[i].teamId}.png`
-                  )
-                );
-                // console.log(data.mapItems[i].iconType + '-' + `${iconWardens.src}`);
-                ctx.drawImage(
-                  iconWardens,
-                  data.mapItems[i].x * 1000 * 1.005,
-                  data.mapItems[i].y * 1000 * 0.85,
-                  24,
-                  24
-                );
-              } else if (data.mapItems[i].teamId == 'COLONIALS') {
-                const iconColonials = await loadImage(
-                  path.resolve(
-                    __dirname,
-                    `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}${data.mapItems[i].teamId}.png`
-                  )
-                );
-                // console.log(
-                //   data.mapItems[i].iconType + '-' + `${iconColonials.src}`
-                // );
-                ctx.drawImage(
-                  iconColonials,
-                  data.mapItems[i].x * 1000 * 1.005,
-                  data.mapItems[i].y * 1000 * 0.85,
-                  24,
-                  24
-                );
-              } else {
-                const iconNoTeam = await loadImage(
-                  path.resolve(
-                    __dirname,
-                    `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}.png`
-                  )
-                );
-                // console.log(data.mapItems[i].iconType + '-' + `${iconNoTeam.src}`);
-                ctx.drawImage(
-                  iconNoTeam,
-                  data.mapItems[i].x * 1000 * 1.005,
-                  data.mapItems[i].y * 1000 * 0.85,
-                  24,
-                  24
-                );
-              }
-            }
-
-            await send(canvas, new Date(data.lastUpdated));
-          } else if (response.statusCode == 304) {
-            console.log(
-              `Api request for: getMap, Response Code: ${response.statusCode}, File: Up-to Date!`
-            );
-
-            const data = cache;
-
-            const canvas = createCanvas(1024, 888);
-            const ctx = canvas.getContext('2d');
-            const map = await loadImage(
-              path.resolve(
-                __dirname,
-                `../assets/Images/MapsPNG/Map${args[0]}.png`
-              )
-            );
-            ctx.drawImage(map, 0, 0, canvas.width, canvas.height);
-
-            for (var i = 0; i < data.mapItems.length; i++) {
-              if (data.mapItems[i].teamId == 'WARDENS') {
-                const iconWardens = await loadImage(
-                  path.resolve(
-                    __dirname,
-                    `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}${data.mapItems[i].teamId}.png`
-                  )
-                );
-                // console.log(data.mapItems[i].iconType + '-' + `${iconWardens.src}`);
-                ctx.drawImage(
-                  iconWardens,
-                  data.mapItems[i].x * 1000 * 1.005,
-                  data.mapItems[i].y * 1000 * 0.85,
-                  24,
-                  24
-                );
-              } else if (data.mapItems[i].teamId == 'COLONIALS') {
-                const iconColonials = await loadImage(
-                  path.resolve(
-                    __dirname,
-                    `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}${data.mapItems[i].teamId}.png`
-                  )
-                );
-                // console.log(
-                //   data.mapItems[i].iconType + '-' + `${iconColonials.src}`
-                // );
-                ctx.drawImage(
-                  iconColonials,
-                  data.mapItems[i].x * 1000 * 1.005,
-                  data.mapItems[i].y * 1000 * 0.85,
-                  24,
-                  24
-                );
-              } else {
-                const iconNoTeam = await loadImage(
-                  path.resolve(
-                    __dirname,
-                    `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}.png`
-                  )
-                );
-                // console.log(data.mapItems[i].iconType + '-' + `${iconNoTeam.src}`);
-                ctx.drawImage(
-                  iconNoTeam,
-                  data.mapItems[i].x * 1000 * 1.005,
-                  data.mapItems[i].y * 1000 * 0.85,
-                  24,
-                  24
-                );
-              }
-            }
-            await send(canvas, new Date(cache.lastUpdated));
-          }
+        const get = axios.get(dynamicMapDataUrl, {
+          validateStatus: function (status) {
+            return status < 500;
+          },
+          headers: { 'If-None-Match': `"${cache.version}"` },
         });
+
+        const data: any = (await get).data;
+        const statusCode: any = (await get).status;
+
+        if (statusCode == 404) {
+          msg.reply({
+            content: 'There was nothing found for the Map/Hex Specified!',
+          });
+        } else if (statusCode == 200) {
+          console.log(
+            `Api request for: getMap, Response Code: ${statusCode}, File: Not Up-to Date!`
+          );
+
+          serverConfig.findOne(
+            { guildID: msg.guildId },
+            (err: any, mongoDB: any) => {
+              fs.writeFile(
+                path.resolve(
+                  __dirname,
+                  `../cache/getMap/${args[0]}${mongoDB.shardName}.json`
+                ),
+                JSON.stringify(data, null, 2),
+                'utf-8',
+                (err) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    console.log('File: Updated!');
+                  }
+                }
+              );
+            }
+          );
+
+          const ctx = canvas.getContext('2d');
+          const map = await loadImage(
+            path.resolve(
+              __dirname,
+              `../assets/Images/MapsPNG/Map${args[0]}.png`
+            )
+          );
+          ctx.drawImage(map, 0, 0, canvas.width, canvas.height);
+
+          for (var i = 0; i < data.mapItems.length; i++) {
+            if (data.mapItems[i].teamId == 'WARDENS') {
+              const iconWardens = await loadImage(
+                path.resolve(
+                  __dirname,
+                  `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}${data.mapItems[i].teamId}.png`
+                )
+              );
+              // console.log(data.mapItems[i].iconType + '-' + `${iconWardens.src}`);
+              ctx.drawImage(
+                iconWardens,
+                data.mapItems[i].x * 1000 * 1.005,
+                data.mapItems[i].y * 1000 * 0.85,
+                24,
+                24
+              );
+            } else if (data.mapItems[i].teamId == 'COLONIALS') {
+              const iconColonials = await loadImage(
+                path.resolve(
+                  __dirname,
+                  `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}${data.mapItems[i].teamId}.png`
+                )
+              );
+              // console.log(
+              //   data.mapItems[i].iconType + '-' + `${iconColonials.src}`
+              // );
+              ctx.drawImage(
+                iconColonials,
+                data.mapItems[i].x * 1000 * 1.005,
+                data.mapItems[i].y * 1000 * 0.85,
+                24,
+                24
+              );
+            } else {
+              const iconNoTeam = await loadImage(
+                path.resolve(
+                  __dirname,
+                  `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}.png`
+                )
+              );
+              // console.log(data.mapItems[i].iconType + '-' + `${iconNoTeam.src}`);
+              ctx.drawImage(
+                iconNoTeam,
+                data.mapItems[i].x * 1000 * 1.005,
+                data.mapItems[i].y * 1000 * 0.85,
+                24,
+                24
+              );
+            }
+          }
+          await send(canvas, new Date(cache.lastUpdated));
+        } else if (statusCode == 304) {
+          console.log(
+            `Api request for: getMap, Response Code: ${statusCode}, File: Up-to Date!`
+          );
+
+          const data = cache;
+
+          const ctx = canvas.getContext('2d');
+          const map = await loadImage(
+            path.resolve(
+              __dirname,
+              `../assets/Images/MapsPNG/Map${args[0]}.png`
+            )
+          );
+          ctx.drawImage(map, 0, 0, canvas.width, canvas.height);
+
+          for (var i = 0; i < data.mapItems.length; i++) {
+            if (data.mapItems[i].teamId == 'WARDENS') {
+              const iconWardens = await loadImage(
+                path.resolve(
+                  __dirname,
+                  `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}${data.mapItems[i].teamId}.png`
+                )
+              );
+              // console.log(data.mapItems[i].iconType + '-' + `${iconWardens.src}`);
+              ctx.drawImage(
+                iconWardens,
+                data.mapItems[i].x * 1000 * 1.005,
+                data.mapItems[i].y * 1000 * 0.85,
+                24,
+                24
+              );
+            } else if (data.mapItems[i].teamId == 'COLONIALS') {
+              const iconColonials = await loadImage(
+                path.resolve(
+                  __dirname,
+                  `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}${data.mapItems[i].teamId}.png`
+                )
+              );
+              // console.log(
+              //   data.mapItems[i].iconType + '-' + `${iconColonials.src}`
+              // );
+              ctx.drawImage(
+                iconColonials,
+                data.mapItems[i].x * 1000 * 1.005,
+                data.mapItems[i].y * 1000 * 0.85,
+                24,
+                24
+              );
+            } else {
+              const iconNoTeam = await loadImage(
+                path.resolve(
+                  __dirname,
+                  `../assets/Images/MapIconsPNG/${data.mapItems[i].iconType}.png`
+                )
+              );
+              // console.log(data.mapItems[i].iconType + '-' + `${iconNoTeam.src}`);
+              ctx.drawImage(
+                iconNoTeam,
+                data.mapItems[i].x * 1000 * 1.005,
+                data.mapItems[i].y * 1000 * 0.85,
+                24,
+                24
+              );
+            }
+          }
+          await send(canvas, new Date(cache.lastUpdated));
+        }
       } catch (err) {
         console.log(err);
         msg.author
